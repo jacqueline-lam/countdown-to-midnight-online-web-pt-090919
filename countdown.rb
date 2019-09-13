454998e365df8b2e689b75816393e12979cf0ebb
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-13 21:43:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,11 +12,11 @@ def countdown(number)
 end
 
 
-def countdown_with_sleep
-  while number < 0 
-    puts "#{number} SECOND(S)!"
-    number -= 1
-    sleep(1)
-  end
-  p "HAPPY NEW YEAR!"
-end
+# def countdown_with_sleep
+#   while number < 0 
+#     puts "#{number} SECOND(S)!"
+#     number -= 1
+#     sleep(1)
+#   end
+#   p "HAPPY NEW YEAR!"
+# end
